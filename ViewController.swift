e58312ff7f5c77b72544c55fcd5855b06f3ea745
tml104 <tml104@txstate.edu>
2021-06-17 17:55:55 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StanApp
+//
+//  Created by Tyler Lawhorn on 6/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
